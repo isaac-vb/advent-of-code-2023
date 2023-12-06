11477df6f55b2ad16713e47c0fa5dd5fe84d5844
Isaac Van Bueren <isaacvanbueren@hotmail.co.uk>
2023-12-06 03:11:33 +0000

    Part 2 Complete

--- day4/day4.py
+++ day4/day4.py
@@ -1,14 +1,6 @@
-# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+
 
 import re
-# Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
-# Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
-# Card 4 has one winning number (84), so it is worth 1 point.
-# Card 5 has no winning numbers, so it is worth no points.
-# Card 6 has no winning numbers, so it is worth no points.
-# So, in this example, the Elf's pile of scratchcards is worth 13 points.
 
 input = "Card 14: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
 input = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"
@@ -27,14 +19,12 @@ def get_info_from_card(card):
     numbers = "".join(card).split("|")
     winning_numbers = [int(n) for n in re.findall(r'\d+', numbers[0])]
     played_numbers = [int(n) for n in re.findall(r'\d+', numbers[1])]
-
-    return {"card_number": card_number, "winning_numbers": winning_numbers, "played_numbers": played_numbers}
-
-
-def calculate_points_for_card(winning_numbers, played_numbers):
-
     matched_numbers = [n for n in played_numbers if n in winning_numbers]
 
+    return {"card_number": card_number, "winning_numbers": winning_numbers, "played_numbers": played_numbers, "matched_numbers": matched_numbers}
+
+
+def calculate_points_for_card(matched_numbers):
     if len(matched_numbers) == 1:
         return 1
     elif len(matched_numbers) > 1:
@@ -46,10 +36,39 @@ def calculate_points_for_card(winning_numbers, played_numbers):
 total_points = 0
 for card in puzzle_input:
     card_info = get_info_from_card(card)
-    winning_numbers = card_info.get("winning_numbers")
-    played_numbers = card_info.get("played_numbers")
+    matched_numbers = card_info.get("matched_numbers")
 
-    total_points += calculate_points_for_card(winning_numbers, played_numbers)
+    total_points += calculate_points_for_card(matched_numbers)
 
 
 print(f"Part 1 Answer: {total_points}")
+
+# Part 2
+
+
+def check_copies_won(card_info):
+    number_of_matches = len(card_info.get('matched_numbers'))
+    card_number = card_info.get('card_number')
+    if number_of_matches != 0:
+        return list(range(card_number+1, card_number + number_of_matches+1))
+
+    return 0
+
+
+puzzle_input = [get_info_from_card(c) for c in puzzle_input]
+
+
+for p in puzzle_input:
+    p['copies'] = 1
+
+for card in puzzle_input:
+    copies_won = check_copies_won(card)
+    if copies_won != 0:
+        for c in copies_won:
+            for entry in puzzle_input:
+                if entry.get('card_number') == c:
+                    entry['copies'] += card['copies']
+
+
+total_scratchcards = sum(entry["copies"] for entry in puzzle_input)
+print(f"Part 2 Answer: {total_scratchcards}")
